a55b97e95e8356094cc8aef892553b9e198d1432
Even <even.berhane2022@gmail.com>
2024-12-03 16:10:08 +0300

    custom bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       {/* Main Content */}
       <div className="flex flex-col justify-center items-center h-full">
         <h1 className="text-5xl font-bold mb-4">Welcome to ChainLens</h1>
-        <p className="text-lg text-gray-400">Track wallet activities and trends like never before.</p>
+        <p className="text-lg text-gray-200">Track wallet activities and trends </p>
       </div>
     </div>
   );
