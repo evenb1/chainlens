42380a6fa0f8b2d98beac966de8aeffcc44e015c
Even <even.berhane2022@gmail.com>
2024-12-04 08:42:27 +0300

    custom header gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
         <Header/>
       {/* Main Content */}
       <div className="flex flex-col p-20 items-center h-full">
-        <h1 className="text-5xl font-bold mb-2 ">Welcome to ChainLens</h1>
-        <p className="text-lg font-light text-gray-400 pb-16">Track wallet activities and trends </p>
+        <h1 className="text-5xl font-bold bg-gradient-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent">Welcome to ChainLens</h1>
+        <p className="text-lg font-light text-slate-500 pb-16">Track wallet activities and trends </p>
         <WalletInput/>
       </div>
     </div>
