08dff7e075837c676fc97046c61c85d8e32639f5
Even <even.berhane2022@gmail.com>
2024-12-14 10:20:51 +0300

    fetch token balance

--- src/app/api/getWalletData.js
+++ src/app/api/getWalletData.js
@@ -7,5 +7,18 @@ export default async function handler(req, res) {
     const connection = new Connection("https://api.mainnet-beta.solana.com");
     const publicKey = new PublicKey(walletAddress);
 
-
+    // Fetch token balances
+    const tokenAccounts = await connection.getParsedTokenAccountsByOwner(publicKey, {
+        programId: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
+      });
+  
+      const balances = tokenAccounts.value.map((account) => {
+        const tokenInfo = account.account.data.parsed.info.tokenAmount;
+        const mintAddress = account.account.data.parsed.info.mint;
+        return {
+          mintAddress,
+          amount: tokenInfo.uiAmount || 0,
+        };
+      });
+  
 }
