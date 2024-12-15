2ceee1d0e4d309399751687438af1e78b1d683f8
Even <even.berhane2022@gmail.com>
2024-12-15 09:49:43 +0300

    footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="mt-8 p-4 bg-neutral-800 text-center text-slate-400">
+       
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
