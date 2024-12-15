6db8df945fd3c4762ccaaa50eed0f50094be6b8d
Even <even.berhane2022@gmail.com>
2024-12-15 10:14:49 +0300

    screen div dimensions fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className=''>
       <body className="relative h-screen w-screen ">
       {children}
-      <Footer/>
 
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import { BackgroundBeamsWithCollision } from "./components/ui/background-beams-w
 export default function Home() {
   return (
     <BackgroundBeamsWithCollision>
-      <div className="relative h-screen w-screen bg-gradient-to-br from-black to-black text-white">
+      <div className="relative flex flex-col h-screen w-screen text-white">
         {/* Header */}
         <Header />
 
-        {/* Highlight Section */}
-        <div className="relative z-20 flex flex-col items-center justify-center pt-20 px-6 text-center">
+        {/* Main Content */}
+        <div className="flex flex-1 flex-col items-center justify-center px-6 text-center">
+          {/* Highlight Section */}
           <h2 className="text-2xl md:text-4xl lg:text-7xl font-bold text-black dark:text-white font-sans tracking-tight">
             What&apos;s cooler than Beams?{" "}
             <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
@@ -23,14 +24,6 @@ export default function Home() {
               </div>
             </div>
           </h2>
-        </div>
-
-        {/* Main Content */}
-        <div className="flex flex-col items-center justify-center pt-16 px-6">
-          {/* Title */}
-          <h1 className="text-5xl font-extrabold bg-gradient-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent">
-            Welcome to ChainLens
-          </h1>
 
           {/* Subtitle */}
           <p className="text-lg font-light text-slate-400 mt-4">
