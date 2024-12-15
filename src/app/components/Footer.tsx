a1421a7f4cb563aef42d0acf4ffc10f099b3ce5e
Even <even.berhane2022@gmail.com>
2024-12-15 09:49:53 +0300

    populate footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer: React.FC = () => {
     return (
       <footer className="mt-8 p-4 bg-neutral-800 text-center text-slate-400">
-       
+        <p>&copy; {new Date().getFullYear()} ChainLens. All rights reserved.</p>
+        <p className="mt-2">
+          Built by <a href="https://github.com/evenb1" className="text-violet-500 hover:underline">Even Russom</a>
+        </p>
       </footer>
     );
   };
